a1ffaa016c913bdd25c1003666b948b047d3029b
rykci <yuen.ricky@hotmail.com>
2021-11-29 12:16:26 -0500

    added MCP config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,8 @@ require('./tasks/tokenURI')
 require('./tasks/pack')
 require('./tasks/zip')
 require('./tasks/deploy')
-//require('./tasks/dev')
+require('./tasks/login')
+require('./tasks/mcp')
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -22,12 +23,26 @@ module.exports = {
     mumbai: {
       url: process.env.MUMBAI_URL,
       accounts: [process.env.PRIVATE_KEY],
-      contract: '0x33317205b442C98C276B3F51EEbC84c6B2F8c9dF',
+      contract: '',
+      mcp: {
+        email: process.env.MCP_EMAIL,
+        password: process.env.MCP_PASSWORD,
+        authToken: process.env.MCP_AUTH_TOKEN,
+        loginUrl: process.env.MCP_LOGIN_URL,
+        uploadUrl: process.env.MCP_UPLOAD_URL,
+      },
     },
     rinkeby: {
       url: process.env.RINKEBY_URL,
       accounts: [process.env.PRIVATE_KEY],
-      contract: '0xa03ab8D39BBBfE6e9B47FA219B5D0663DB7D67Fa',
+      contract: '',
+      mcp: {
+        email: process.env.MCP_EMAIL,
+        password: process.env.MCP_PASSWORD,
+        authToken: process.env.MCP_AUTH_TOKEN,
+        loginUrl: process.env.MCP_LOGIN_URL,
+        uploadUrl: process.env.MCP_UPLOAD_URL,
+      },
     },
   },
 }
