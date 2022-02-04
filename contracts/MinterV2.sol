f29791f29114e2dfc3d82b923005f9931178028c
rykci <yuen.ricky@hotmail.com>
2022-02-04 14:45:20 -0500

    Add Minter.sol with test cases

---
+++ contracts/MinterV2.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+
+//////////////////////////////////////////////
+// This contract is only used for testing!! //
+//////////////////////////////////////////////
+contract MinterV2 is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    string public baseURI;
+    string public contractURI;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    //constructor() initializer {}
+
+    function initialize(string memory _name, string memory _symbol) initializer public {
+        __ERC721_init(_name, _symbol);
+        __ERC721URIStorage_init();
+        __Ownable_init();
+    }
+
+    function mintData(address minter, string memory uri)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(minter, tokenId);
+        _setTokenURI(tokenId, string(abi.encodePacked("<URI PREFIX>:", uri)));
+
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    // setter functions
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+
+    function setContractURI(string memory _newContractURI) public onlyOwner {
+        contractURI = _newContractURI;
+    }
+
+    // get the current supply of tokens
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+}
---
+++ test/Minter.js
@@ -0,0 +1,65 @@
+const { expect } = require('chai')
+const { ethers, upgrades } = require('hardhat')
+
+let minter
+let Minter
+let owner
+let addr1
+let addr2
+let addrs
+
+// deploy contract
+before(async () => {
+  ;[owner, addr1, addr2, ...addrs] = await ethers.getSigners()
+
+  Minter = await ethers.getContractFactory('Minter')
+  minter = await upgrades.deployProxy(Minter, ['Test Token', 'TTKN'])
+
+  await minter.deployed()
+})
+
+describe('Minter', function () {
+  it('New contract instance should have no minted tokens', async function () {
+    const totalSupply = (await minter.totalSupply()).toString()
+
+    expect(totalSupply).to.equal('0')
+  })
+
+  it('Should be able to mint token', async () => {
+    const tokenURI = 'test'
+    const mintTx = await minter.mintData(owner.address, tokenURI)
+
+    await mintTx.wait()
+
+    const totalSupply = (await minter.totalSupply()).toString()
+
+    expect(totalSupply).to.equal('1')
+  })
+
+  it('Should be able to view token URI', async () => {
+    expect(await minter.tokenURI(1)).to.equal('test')
+  })
+
+  it('Other users should not be able to mint token', async () => {
+    const tokenURI = "addr1's nft"
+
+    await expect(
+      minter.connect(addr1).mintData(owner.address, tokenURI),
+    ).to.be.revertedWith('Ownable: caller is not the owner')
+  })
+
+  it('Should be upgradeable', async () => {
+    const tokenURI = 'new uri'
+
+    const MinterV2 = await ethers.getContractFactory('MinterV2')
+    await upgrades.upgradeProxy(minter.address, MinterV2)
+
+    const mintTx = await minter.mintData(owner.address, tokenURI)
+    await mintTx.wait()
+
+    const totalSupply = (await minter.totalSupply()).toString()
+
+    expect(totalSupply).to.equal('2')
+    expect(await minter.tokenURI(2)).to.equal('<URI PREFIX>:new uri')
+  })
+})
