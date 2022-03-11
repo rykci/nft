3bfbf98f3af95e1f8ad5e956329e631bd5797bd4
rykci <yuen.ricky@hotmail.com>
2022-03-11 17:35:40 -0500

    change max length and logs

--- tasks/batchUpload.js
+++ tasks/batchUpload.js
@@ -23,6 +23,8 @@ const uploadPromise = (
     `${network.config.mcs_api}/storage/ipfs/upload`,
     form,
     {
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
       headers: {
         ...form.getHeaders(),
       },
@@ -38,24 +40,25 @@ task('batchUpload', 'upload files from csv file to MCS')
   .setAction(async ({ file, delay }) => {
     const signer = await ethers.getSigner() // get signer for wallet address
     let csvArray = await csv().fromFile(file) // read csv
-    // read file from filePath in csv
-    csvArray = await Promise.all(
-      csvArray.map(async (row) => ({
-        ...row,
-        file: await fs.readFile(row.filePath),
-      })),
+
+    // read files from filePaths in csv
+    const files = await Promise.all(
+      csvArray.map((row) => fs.readFile(row.filePath)),
     )
 
     const delayIncrement = parseInt(delay) || 1000
     let apiDelay = 0
 
+    console.log(`Preparing to upload ${files.length} files...`)
+
     // send post request for each row (with delay)
-    const requests = csvArray.map((row) => {
+    const requests = csvArray.map((row, i) => {
       apiDelay += delayIncrement // staggers each api call
       return new Promise((resolve) => setTimeout(resolve, apiDelay)).then(() =>
-        uploadPromise(row.fileName, row.file, signer.address).then(
-          (res) => res.data,
-        ),
+        uploadPromise(row.fileName, files[i], signer.address).then((res) => {
+          console.log(`file ${i + 1} uploaded`)
+          return res.data
+        }),
       )
     })
 
@@ -63,6 +66,6 @@ task('batchUpload', 'upload files from csv file to MCS')
       const result = await Promise.all(requests) // wait for all uploads
       console.log(result)
     } catch (err) {
-      console.log(err.response.data)
+      console.log(err.response?.data || err)
     }
   })
