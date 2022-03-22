b4938f1b450a7adb7bbebccf4b6a6397d1be3296
rykci <yuen.ricky@hotmail.com>
2022-03-22 19:03:16 -0400

    add docs and examples

--- client-test/test.js
+++ client-test/test.js
@@ -1,45 +1,19 @@
-const { mcsClient } = require('mcs-client')
+require('dotenv').config('./.env')
 const fs = require('fs').promises
+const { mcsClient } = require('mcs-client')
+const client = new mcsClient({
+  privateKey: process.env.PRIVATE_KEY,
+  rpcUrl: process.env.RPC_URL,
+})
 
-const test = async () => {
-  const mcs = new mcsClient({
-    privateKey: '<PRIVATE_KEY>',
-    rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/<API_KEY>',
-  })
-
-  const filesArray = [
-    { fileName: 'Filswan Logo', file: await fs.readFile('./filswan.jpeg') },
+async function main() {
+  const fileArray = [
+    { fileName: 'file1', file: await fs.readFile('./file1.txt') },
   ]
-  console.log('VERSION: ', mcs.version)
 
-  console.log('UPLOADING...')
-  const uploadRes = await mcs.upload(filesArray)
-  console.log(uploadRes)
-
-  const responseData = uploadRes[0].data // use the first file as example
-
-  if (responseData.need_pay == 0 || responseData.need_pay == 2) {
-    // make payment
-    console.log('LOCKING TOKENS')
-    const tx = await mcs.makePayment(responseData.payload_cid, '5')
-    console.log(tx.transactionHash)
-  }
-
-  /*
-  console.log('CHECK STATUS')
-  const tx2 = await mcs.checkStatus(testResponse.payload_cid)
-  console.log(tx2)
-
-  console.log('MINTING NFT...')
-  const tx3 = await mcs.mintAsset(testResponse.payload_cid, {
-    name: 'test',
-    image: testResponse.ipfs_url,
-  })
-  console.log(tx3)
-
-  console.log('LISTING UPLOADS')
-  console.log(await mcs.listUploads())
-  */
+  const uploadResponse = await client.upload(fileArray)
+  console.log(uploadResponse[0].data.ipfs_url)
+  // https://calibration-ipfs.filswan.com/ipfs/QmPjwPk7xiQWVA3VLoK9F9nk2cL7oE2LRFU6jzLwc9cnQm
 }
 
-test()
+main()
